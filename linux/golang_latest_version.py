837b6023203ccacc00205849f14008514e691384
Ezekiel N Bukari <enbukari@gmail.com>
2021-06-14 08:14:53 +0000

    cleanup golang unused files On branch goinstall Your branch is up to date with 'origin/goinstall'.
    Changes to be committed:
    	modified:   linux/golang_latest_version.py
    	modified:   linux/install_go.sh
    
    Changes not staged for commit:
    	modified:   linux/new_install.sh
    
    Untracked files:
    	bash/
    	git/.gitconfig
    	linux/extras.sh
    	linux/install_lua.sh
    	linux/lua_latest.py
    	vscode/
    
    - Removed unused variables
    - Echo some variables to track progress of files
    - Follow golang install guidelines
    
    - Add golang to $PATH

--- linux/golang_latest_version.py
+++ linux/golang_latest_version.py
@@ -4,18 +4,21 @@ import re
 import urllib.request
 from html.parser import HTMLParser
 
-GO_RELEASES_URL = r'https://golang.org/doc/devel/release'
-GO_VERSION_RE = re.compile(r'''^go\d     # go and major version number
-                               \.
-                               \d+       # major release number
-                               (\.\d+)*  # minor rlease number
-                               $''', re.X | re.I)
-GO_MAJOR_VERSION_RE = re.compile(r'^(Go \d\.\d+) is a major')
+GO_RELEASES_URL = r"https://golang.org/doc/devel/release"
+GO_VERSION_RE = re.compile(
+    r"""^go\d     # go and major version number
+        \.
+        \d+       # major release number
+        (\.\d+)*  # minor rlease number
+        $""",
+    re.X | re.I,
+)
+GO_MAJOR_VERSION_RE = re.compile(r"^(Go \d\.\d+) is a major")
 
 
 def get_url(url):
     page = urllib.request.urlopen(url)
-    page_source = page.read().decode('utf-8')
+    page_source = page.read().decode("utf-8")
     return page_source
 
 
@@ -27,7 +30,6 @@ def get_golang_latest():
     page_source = get_url(GO_RELEASES_URL)
 
     class GolangParser(HTMLParser):
-
         def __init__(self, *args, **kwargs):
             super(GolangParser, self).__init__(*args, **kwargs)
             self._versions = []
@@ -39,7 +41,7 @@ def get_golang_latest():
                     self._versions.append(version_str_to_tuple(line[2:]))
 
                 elif GO_MAJOR_VERSION_RE.match(line) is not None:
-                    version_string = line.split(' ')[1]
+                    version_string = line.split(" ")[1]
                     self._versions.append(version_str_to_tuple(version_string))
 
         def get_latest_version(self):
--- linux/install_go.sh
+++ linux/install_go.sh
@@ -3,11 +3,6 @@
 set -e
 
 VERSION="$(./golang_latest_version.py)"
-
-
-[ -z "$GOROOT" ] && GOROOT="/usr/local/go"
-[ -z "$GOPATH" ] && GOPATH="$HOME/go"
-
 OS="$(uname -s)"
 ARCH="$(uname -m)"
 
@@ -37,122 +32,38 @@ case $OS in
     ;;
 esac
 
-print_help() {
-    echo "Usage: bash goinstall.sh OPTIONS"
-    echo -e "\nOPTIONS:"
-    echo -e "  --remove\tRemove currently installed version"
-    echo -e "  --version\tSpecify a version number to install"
-}
-
 if [ -z "$PLATFORM" ]; then
     echo "Your operating system is not supported by the script."
     exit 1
 fi
 
-if [ -n "$($SHELL -c 'echo $ZSH_VERSION')" ]; then
-    shell_profile="$HOME/.zshrc"
-elif [ -n "$($SHELL -c 'echo $BASH_VERSION')" ]; then
-    shell_profile="$HOME/.bashrc"
-elif [ -n "$($SHELL -c 'echo $FISH_VERSION')" ]; then
-    shell="fish"
-    if [ -d "$XDG_CONFIG_HOME" ]; then
-        shell_profile="$XDG_CONFIG_HOME/fish/config.fish"
-    else
-        shell_profile="$HOME/.config/fish/config.fish"
-    fi
-fi
+PACKAGE_NAME="go$VERSION.$PLATFORM.tar.gz"
+TEMP_DIRECTORY=$(mktemp -d)
 
-# if [ "$1" == "--remove" ]; then
-#     rm -rf "$GOROOT"
-#     if [ "$OS" == "Darwin" ]; then
-#         if [ "$shell" == "fish" ]; then
-#             sed -i "" '/# GoLang/d' "$shell_profile"
-#             sed -i "" '/set GOROOT/d' "$shell_profile"
-#             sed -i "" '/set GOPATH/d' "$shell_profile"
-#             sed -i "" '/set PATH $GOPATH\/bin $GOROOT\/bin $PATH/d' "$shell_profile"
-#         else
-#             sed -i "" '/# GoLang/d' "$shell_profile"
-#             sed -i "" '/export GOROOT/d' "$shell_profile"
-#             sed -i "" '/$GOROOT\/bin/d' "$shell_profile"
-#             sed -i "" '/export GOPATH/d' "$shell_profile"
-#             sed -i "" '/$GOPATH\/bin/d' "$shell_profile"
-#         fi
-#     else
-#         if [ "$shell" == "fish" ]; then
-#             sed -i '/# GoLang/d' "$shell_profile"
-#             sed -i '/set GOROOT/d' "$shell_profile"
-#             sed -i '/set GOPATH/d' "$shell_profile"
-#             sed -i '/set PATH $GOPATH\/bin $GOROOT\/bin $PATH/d' "$shell_profile"
-#         else
-#             sed -i '/# GoLang/d' "$shell_profile"
-#             sed -i '/export GOROOT/d' "$shell_profile"
-#             sed -i '/$GOROOT\/bin/d' "$shell_profile"
-#             sed -i '/export GOPATH/d' "$shell_profile"
-#             sed -i '/$GOPATH\/bin/d' "$shell_profile"
-#         fi
-#     fi
-#     echo "Go removed."
-#     exit 0
-# elif [ "$1" == "--help" ]; then
-#     print_help
-#     exit 0
-# elif [ "$1" == "--version" ]; then
-#     if [ -z "$2" ]; then # Check if --version has a second positional parameter
-#         echo "Please provide a version number for: $1"
-#     else
-#         VERSION=$2
-#     fi
-# elif [ ! -z "$1" ]; then
-#     echo "Unrecognized option: $1"
-#     exit 1
-# fi
+echo "PACKAGE_NAME: $PACKAGE_NAME"
+echo "TEMP_DIRECTORY: $TEMP_DIRECTORY"
 
-# if [ -d "$GOROOT" ]; then
-#     echo "The Go install directory ($GOROOT) already exists. Exiting."
-#     exit 1
-# fi
-
-# PACKAGE_NAME="go$VERSION.$PLATFORM.tar.gz"
-# TEMP_DIRECTORY=$(mktemp -d)
+PACKAGE_PATH="$TEMP_DIRECTORY/$PACKAGE_NAME"
 
 echo "Downloading $PACKAGE_NAME ..."
+echo "PACKAGE_PATH: $PACKAGE_PATH"
 if hash wget 2>/dev/null; then
-    wget https://storage.googleapis.com/golang/$PACKAGE_NAME -O "$TEMP_DIRECTORY/go.tar.gz"
+    echo "Using wget........\n"
+    wget "https://golang.org/dl/$PACKAGE_NAME" -O "$PACKAGE_PATH"
 else
-    curl -o "$TEMP_DIRECTORY/go.tar.gz" https://storage.googleapis.com/golang/$PACKAGE_NAME
+    echo "Using curl.............\n"
+    curl -o "$PACKAGE_PATH" "https://golang.org/dl/$PACKAGE_NAME"
 fi
+
 
 if [ $? -ne 0 ]; then
     echo "Download failed! Exiting."
     exit 1
 fi
 
+echo "Contents of temp directory: "
+ls $TEMP_DIRECTORY
+
+
 echo "Extracting File..."
-mkdir -p "$GOROOT"
-
-tar -C "$GOROOT" --strip-components=1 -xzf "$TEMP_DIRECTORY/go.tar.gz"
-
-echo "Configuring shell profile in: $shell_profile"
-touch "$shell_profile"
-if [ "$shell" == "fish" ]; then
-    {
-        echo '# GoLang'
-        # echo "set GOROOT '${GOROOT}'"
-        echo "set GOPATH '$GOPATH'"
-        echo 'set PATH $GOPATH/bin' # $GOROOT/bin $PATH'
-    } >> "$shell_profile"
-else
-    {
-        echo '# GoLang'
-        # echo "export GOROOT=${GOROOT}"
-        # echo 'export PATH=$GOROOT/bin:$PATH'
-        echo "export GOPATH=$GOPATH"
-        echo 'export PATH=$GOPATH/bin:$PATH'
-    } >> "$shell_profile"
-fi
-
-mkdir -p "${GOPATH}/"{src,pkg,bin}
-echo -e "\nGo $VERSION was installed into $GOROOT.\nMake sure to relogin into your shell or run:"
-echo -e "\n\tsource $shell_profile\n\nto update your environment variables."
-echo "Tip: Opening a new terminal window usually just works. :)"
-rm -f "$TEMP_DIRECTORY/go.tar.gz"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "$PACKAGE_PATH"
